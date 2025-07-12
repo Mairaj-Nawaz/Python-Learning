261d0a9e28f95cc8c6c52ce189580f66997f1965
Mairaj-Nawaz <nizamanimaraj@gmail.com>
2025-07-12 11:42:07 +0500

    task4

--- Day4/task4.py
+++ Day4/task4.py
@@ -5,12 +5,25 @@
 
 # After the loop, print the list.
 
-list = []
-count = 0
+# list = []
+# count = 0
+# square = 0
 
-for i in (list):
-    count += 1
-    list += count
-    print(count)
-    print(list)
-print(list)
+# for i in range(1, 6):
+#     count += 1
+#     square.append(i**2)
+#     print(count)
+#     print(square)
+# print(list)
+
+
+# Create an empty list
+squares = []
+
+# Using a loop from 1 to 5
+for i in range(1, 6):
+    # Append the square of each number to the list
+    squares.append(i ** 2)
+
+# After the loop, print the list
+print(squares)
