a1e0fa2cfd0954102b8779c6a513f4146780360e
Muhammad Ali <113182061+MAliSheikh@users.noreply.github.com>
2025-07-23 21:57:02 +0500

    change task

--- Day6/task3.py
+++ Day6/task3.py
@@ -1,8 +1,8 @@
-# Take two lists in a function and convert them into a set separtely
-# Then merge both sets and return the merged set
+# Take two lists in a function and convert them into a tuple separtely
+# Then merge both tuples and return the merged tuple
 # Requirements:
-# Define a function merge_sets(list1, list2) that:
+# Define a function merge_tuples(list1, list2) that:
 # Takes two lists list1 and list2 as input.
-# Converts both lists into sets.
-# Merges both sets.
-# Returns the merged set.
+# Converts both lists into tuples.
+# Merges both tuples.
+# Returns the merged tuple.
